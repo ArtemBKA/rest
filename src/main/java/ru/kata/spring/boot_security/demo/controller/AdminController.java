561e2bc8b60a27ab18072260f10dbfa3ac45a088
Eugene Strelecky <Macfliner@gmail.com>
2023-05-25 01:51:22 +0300

    Fix

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Controller;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-
     private final UserService userService;
     private final RoleService roleService;
 
@@ -62,65 +61,3 @@ public class AdminController {
         return "redirect:/admin";
     }
 }
-
-
-//@Controller
-//public class AdminController {
-//    private final UserService userService;
-//
-//    public AdminController(UserService userService) {
-//        this.userService = userService;
-//    }
-
-//    @GetMapping("/")
-//    public String printUsers(ModelMap model) {
-//        model.addAttribute("userList", userService.userList());
-//        return "index";
-//    }
-//
-//    @GetMapping("/new")
-//    public String newUser(ModelMap model) {
-//        model.addAttribute("user", new User());
-//        return "new";
-//    }
-//
-//    @PostMapping("/save")
-//    public String saveUser(@ModelAttribute("user") User user) {
-//        userService.save(user);
-//        return "redirect:/";
-//    }
-//
-//    @GetMapping("/search")
-//    public String searchUserById(ModelMap model, @RequestParam(value = "keyword") long id) {
-//        model.addAttribute("user", userService.getUserById(id));
-//        return "search";
-//    }
-//
-//    @GetMapping("/edit")
-//    public String editUser(ModelMap model, @RequestParam long id) {
-//        model.addAttribute("user", userService.getUserById(id));
-//        return "edit";
-//    }
-//
-//    @PatchMapping("/{id}")
-//    public String updateUser(@ModelAttribute("user") User user) {
-//        userService.update(user);
-//        return "redirect:/";
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public String deleteUser(@PathVariable("id") long id) {
-//        userService.delete(id);
-//        return "redirect:/";
-//    }
-//
-//    @GetMapping("/user")
-//    public String user() {
-//        return "user";
-//    }
-//
-//    @GetMapping("/index")
-//    public String index() {
-//        return "index";
-//    }
-//}
