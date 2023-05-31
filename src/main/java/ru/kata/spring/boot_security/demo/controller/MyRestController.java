c9f274299ea2d07b4f807e2982bd1e7608677d4c
Eugene Strelecky <Macfliner@gmail.com>
2023-05-31 13:04:23 +0300

    Fix

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/MyRestController.java
@@ -0,0 +1,64 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
+import ru.kata.spring.boot_security.demo.service.UserService;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyRestController {
+    private final RoleService roleService;
+    private final UserService userService;
+
+    @Autowired
+    public MyRestController(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.userList(), HttpStatus.OK);
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUser(@PathVariable(name ="id") long id) {
+            User user = userService.getUserById(id);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> addNewUser(@RequestBody User user) {
+        userService.save(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/users/{id}")
+    public ResponseEntity<?> update(@RequestBody User user, @PathVariable("id") long id) {
+            User thisUser = userService.getUserById(id);
+            userService.update(user);
+            return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<User> delete(@PathVariable("id") long id) {
+        userService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/roles")
+    ResponseEntity<List<Role>>getAllRoles(){
+        return new ResponseEntity<>(roleService.roleList(), HttpStatus.OK);
+    }
+
+    @GetMapping("/roles/{id}")
+    ResponseEntity<Role> getRoleById(@PathVariable("id") long id){
+        return new ResponseEntity<>(roleService.getRoleById(id), HttpStatus.OK);
+    }
+}
