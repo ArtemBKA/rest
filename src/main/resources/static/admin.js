d47c04ad67592eabc14ebb7602deb2e6c69ef8eb
Eugene Strelecky <Macfliner@gmail.com>
2023-06-01 16:51:32 +0300

    Fix

--- src/main/resources/static/admin.js
+++ src/main/resources/static/admin.js
@@ -44,6 +44,7 @@ const renderUsers = (users) => {
                 <td>${user.lastName}</td>
                 <td>${user.age}</td>
                 <td>${user.email}</td>
+                <td>${user.password}</td>
                 <td>
                  ${roles}
                 </td>
