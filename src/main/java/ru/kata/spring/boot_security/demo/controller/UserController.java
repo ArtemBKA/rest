50d29010a277a793ec835362a45f5e68e3d931fc
Eugene Strelecky <Macfliner@gmail.com>
2023-06-01 15:59:13 +0300

    Fix

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -4,12 +4,15 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/users")
 public class UserController {
-    @GetMapping
+    @GetMapping("/")
+    public String rootBoot() {
+        return "redirect:/login";
+    }
+
+    @GetMapping("/users")
     public String readGet(Model model, Authentication authentication) {
         model.addAttribute("user", authentication.getPrincipal());
         return "users";
