6c34f5b4d2407d9b3d792b0334d138bf8aa8d5ee
Eugene Strelecky <Macfliner@gmail.com>
2023-06-02 17:36:59 +0300

    Add UserRestController and AdminRestController

--- src/main/java/ru/kata/spring/boot_security/demo/controller/ViewController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/ViewController.java
@@ -7,11 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
-    @GetMapping("/")
-    public String rootBoot() {
-        return "redirect:/login";
-    }
-
     @GetMapping("/users")
     public String readGet(Model model, Authentication authentication) {
         model.addAttribute("user", authentication.getPrincipal());
