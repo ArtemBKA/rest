46963a12bc82122039f29e9b65c7aa402bd5a43a
Eugene Strelecky <Macfliner@gmail.com>
2023-06-02 17:20:26 +0300

    Add UserRestController and AdminRestController

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserRestController.java
@@ -0,0 +1,22 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/user")
+public class UserRestController {
+    private final UserService userService;
+
+    public UserRestController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/info")
+    public User oneUser(Principal principal) {
+        return (User) userService.loadUserByUsername(principal.getName());
+    }
+}
---
+++ src/main/resources/static/info.js
@@ -0,0 +1,15 @@
+const urlUser = 'http://localhost:8080/user/info/'
+let loggedInUser = document.querySelector('#UserInfo');
+
+fetch(urlUser)
+    .then(res => res.json())
+    .then(data => {
+        loggedInUser.innerHTML = `
+                                <td>${data.id}</td>
+                                <td>${data.firstName}</td>
+                                <td>${data.lastName}</td>
+                                <td>${data.age}</td>
+                                <td>${data.email}</td>
+                                <td>${data.roles.map(role => role.name === 'ROLE_USER' ? ' USER' : ' ADMIN')}</td>
+                                `;
+    })
