13a8eef15bf9ec2a64e13cc1e3f39929d820b415
Eugene Strelecky <Macfliner@gmail.com>
2023-06-04 16:55:32 +0300

    Fix

--- src/main/resources/static/user.js
+++ src/main/resources/static/user.js
@@ -1,33 +1,42 @@
-const urlUser = 'http://localhost:8080/user/info/'
+const urlUser = 'http://localhost:8080/user/info/';
 let loggedInUser = document.querySelector('#UserInfo');
-
-fetch(urlUser)
-    .then(res => res.json())
-    .then(data => {
-        loggedInUser.innerHTML = `
-                                <td>${data.id}</td>
-                                <td>${data.firstName}</td>
-                                <td>${data.lastName}</td>
-                                <td>${data.age}</td>
-                                <td>${data.email}</td>
-                                <td>${data.roles.map(role => role.name === 'ROLE_USER' ? 'USER' : 'ADMIN').join(' ')}</td>
-                                `;
-    })
-
 let loggedUserEmail = document.querySelector('#EmailInfo');
-fetch(urlUser)
-    .then(res => res.json())
-    .then(data => {
-        loggedUserEmail.innerHTML = `
-                                <td>${data.email}</td>
-                                `;
-    })
-
 let loggedUserRoles = document.querySelector('#RolesInfo');
-fetch(urlUser)
-    .then(res => res.json())
-    .then(data => {
-        loggedUserRoles.innerHTML = `
-                                <td>${data.roles.map(role => role.name === 'ROLE_USER' ? 'USER' : 'ADMIN').join(' ')}</td>
-                                `;
-    })
+
+function getUserData() {
+    fetch(urlUser)
+        .then(res => res.json())
+        .then(data => {
+            loggedInUser.innerHTML = `
+        <td>${data.id}</td>
+        <td>${data.firstName}</td>
+        <td>${data.lastName}</td>
+        <td>${data.age}</td>
+        <td>${data.email}</td>
+        <td>${data.roles
+                .map(role => role.name === 'ROLE_USER' ? 'USER' : 'ADMIN')
+                .sort((a, b) => a.localeCompare(b))
+                .join(' ')
+            }</td>
+      `;
+
+            loggedUserEmail.innerHTML = `
+        <td>${data.email}</td>
+      `;
+
+            loggedUserRoles.innerHTML = `
+        <td>${data.roles
+                .map(role => role.name === 'ROLE_USER' ? 'USER' : 'ADMIN')
+                .sort((a, b) => a.localeCompare(b))
+                .join(' ')
+            }</td>
+      `;
+        })
+        .catch(error => console.error(error));
+}
+
+getUserData();
+
+setInterval(() => {
+    getUserData();
+}, 100);
